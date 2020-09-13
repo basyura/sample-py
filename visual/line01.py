9a7b0827bc5bf1db25dad27c604fb8e8e1328486
basyura <basyura@gmail.com>
2020-09-13 13:03:44 +0900

    improved

--- visual/line01.py
+++ visual/line01.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import japanize_matplotlib
 
 
 # 折れ線グラフを出力
@@ -17,14 +18,12 @@ x = pd.to_datetime(npary[:, 0])
 
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
-ax.plot(x, np.array(npary[:, 1], dtype=np.int64), marker="o")
-ax.plot(x, np.array(npary[:, 2], dtype=np.int64), marker="o")
+ax.grid(c='gainsboro', zorder=9)
 
-# fig.text(0.1, 0.1, '0.1', horizontalalignment='center')
-# fig.text(0.2, 0.2, '0.2', horizontalalignment='center')
+ax.plot(x, np.array(npary[:, 1], dtype=np.int64), marker="o", label="ラベルA")
+ax.plot(x, np.array(npary[:, 2], dtype=np.int64), marker="o", label="ラベルB")
 fig.text(0.5, 0.5, '0.5 - 0.5 text', horizontalalignment='center')
-# fig.text(0.9, 0.9, '0.9', horizontalalignment='center')
-
+# 値をプロット
 for r in records:
     for i in range(len(r) - 1):
         ax.text(r[0], r[i+1] + 10, r[i+1] + 20)
@@ -32,4 +31,5 @@ for r in records:
 ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
 ax.set_ylim([0, 500])
 
+plt.legend()
 plt.savefig('Py.png')
